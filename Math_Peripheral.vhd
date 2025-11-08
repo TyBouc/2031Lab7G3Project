adb9b83cceb8c34fd371e7af208ad27ba429b402
Hadi Benkorah <hadibenkorah@gmail.com>
2025-11-08 13:45:33 -0500

    yup

--- Math_Peripheral.vhd
+++ Math_Peripheral.vhd
@@ -28,4 +28,29 @@ ARCHITECTURE a OF Math_Peripheral IS
     SIGNAL IO_EN : STD_LOGIC;
     SIGNAL IO_OUT : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
-	 Begin --GoFromHere
+	Begin --GoFromHere
+    
+    IO_EN <= IO_READ OR IO_WRITE;
+
+    PROCESS (CLOCK, RESETN)
+    BEGIN
+        IF RESETN = '0' THEN
+            Mult_x1 <= (OTHERS => '0');
+            Mult_x2 <= (OTHERS => '0');
+            Div_num  <= (OTHERS => '0');
+            Div_den  <= (OTHERS => '0');
+            Mod_num  <= (OTHERS => '0');
+            Mod_den  <= (OTHERS => '0');
+            Sin_x    <= (OTHERS => '0');
+            Cos_x    <= (OTHERS => '0');
+            Result_H <= (OTHERS => '0');
+            Result_L <= (OTHERS => '0');
+        ELSIF RISING_EDGE(CLOCK) THEN
+            IF IO_EN = '1' THEN
+                -- Handle IO_WRITE and IO_READ operations here
+                -- (Implementation of read/write logic goes here)
+            END IF;
+        END IF;
+    END PROCESS;
+
+    IO_DATA <= IO_OUT WHEN IO_READ = '1' ELSE (OTHERS => 'Z');
