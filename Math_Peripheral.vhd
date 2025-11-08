c340d39433ef839038f05e8068556647912bf0b2
TyBouc <boucino.ty@gmail.com>
2025-11-08 13:55:12 -0500

    Update Math_Peripheral.vhd

--- Math_Peripheral.vhd
+++ Math_Peripheral.vhd
@@ -19,11 +19,8 @@ END Math_Peripheral;
 ARCHITECTURE a OF Math_Peripheral IS
 
     -- Internal registers
-    SIGNAL Mult_x1, Mult_x2     : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL Div_num, Div_den     : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL Mod_num, Mod_den     : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL Sin_x, Cos_x         : STD_LOGIC_VECTOR(7 DOWNTO 0);
-    SIGNAL Result_H, Result_L   : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL x1, x2    : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL Result   : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
     SIGNAL IO_EN : STD_LOGIC;
     SIGNAL IO_OUT : STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -35,18 +32,19 @@ ARCHITECTURE a OF Math_Peripheral IS
     PROCESS (CLOCK, RESETN)
     BEGIN
         IF RESETN = '0' THEN
-            Mult_x1 <= (OTHERS => '0');
-            Mult_x2 <= (OTHERS => '0');
-            Div_num  <= (OTHERS => '0');
-            Div_den  <= (OTHERS => '0');
-            Mod_num  <= (OTHERS => '0');
-            Mod_den  <= (OTHERS => '0');
-            Sin_x    <= (OTHERS => '0');
-            Cos_x    <= (OTHERS => '0');
-            Result_H <= (OTHERS => '0');
-            Result_L <= (OTHERS => '0');
+            x1 <= (OTHERS => '0');
+            x2 <= (OTHERS => '0');
+            Result <= (OTHERS => '0');
         ELSIF RISING_EDGE(CLOCK) THEN
             IF IO_EN = '1' THEN
+                if (ioaddr = 0x90)
+                    do x1 * x2 -> Result
+                else if (ioaddr = 0x91) and x2 != 0
+                    do x1 / x2 -> Result
+                else if (0x93)
+                    x2 & x1 => y
+                    siny = result
+                
                 -- Handle IO_WRITE and IO_READ operations here
                 -- (Implementation of read/write logic goes here)
             END IF;
