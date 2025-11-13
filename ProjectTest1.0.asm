5669862ec359a7a3f65d59d35d3b31651cdc4293
TyBouc <boucino.ty@gmail.com>
2025-11-13 10:22:59 -0500

    ASM
    FRICK MAN

---
+++ ProjectTest1.0.asm
@@ -0,0 +1,24 @@
+; ProjectTest1.0.asm
+
+ORG 0
+	LOADI &B0000010000000101
+    SHIFT 8
+    OR 10
+    Store Temp
+    OUT Mult
+    IN Result
+    STORE Ans
+    OUT HEX0
+	 LOADI 4
+	 OUT HEX1
+	
+
+
+
+Temp: DW 0
+Ans: DW 0
+
+Mult: EQU &H090
+Result: EQU &H096
+Hex1: EQU 005
+Hex0: EQU 004
